db2ab16cbfa6657fc1f29819677cff529717cd6e
Sebastian Scinteie <sebastian.scinteie@gmail.com>
2023-07-18 12:18:47 +0300

    Remove comments

--- server/index.ts
+++ server/index.ts
@@ -7,15 +7,13 @@ const next = require('next')
 const dev = process.env.NODE_ENV !== 'production'
 const hostname = 'localhost'
 const port = 8080
-// when using middleware `hostname` and `port` must be provided below
+
 const app = next({ dev, hostname, port })
 const handle = app.getRequestHandler()
  
 app.prepare().then(() => {
   createServer(async (req:any, res:any) => {
     try {
-      // Be sure to pass `true` as the second argument to `url.parse`.
-      // This tells it to parse the query portion of the URL.
       const parsedUrl = parse(req.url, true)
       const { pathname, query } = parsedUrl
  
